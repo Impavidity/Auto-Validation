53214afaf71f38d9cb53356e37587e5e87f7b3b9
Yuren Zhou <ervine0519@gmail.com>
2015-11-21 02:25:52 +0800

    updated the validationMain and the way analytic results are obtained
    1. Changed the query source from IHPC to Postgres
    2. Added/modified some data process procedures based on the exact data
    from Postgres
    3. Added the feedback of am_track, pm_track and durations
    4. added some testing results "testRestul1.txt" and"testRestul2.txt" for
    reference

--- NSE_Analytics_for_validation/process.py
+++ NSE_Analytics_for_validation/process.py
@@ -19,7 +19,7 @@ Options:
 
 """
 
-
+import psycopg2
 import requests
 import json
 import httplib
@@ -43,7 +43,7 @@ from util import great_circle_dist, aveWithNan
 
 API_key = "sutd-nse_api:dj6M9RAxynrjw9aWztzprfh5AKHssgVj4qKXiKSfHRyGKeoX92wmwmEJKpHMIB5"
 
-def getData(url, nid, start_time=0, end_time=int(time.time()), table=None):
+def getDataIHPC(url, nid, start_time=0, end_time=int(time.time()), table=None):
     """Retrieve raw hardware data for device nid for the specified time
     frame and specified table if specified.  Return a pandas data frame of
     measurements or None if no data was returned.
@@ -67,131 +67,36 @@ def getData(url, nid, start_time=0, end_time=int(time.time()), table=None):
         logging.warning("getData for " +str(nid) + " for end: " + str(end_time) + " returned %s" % resp["error"])
         return None
 
-def getStatus(url, nid, date):
-    """Get the processed/unprocessed status for device nid on the
-    specified date (date format 'YYYY-MM-dd') using the specified API
-    url. Return True (processed) or False (not processed yet).
-
+def getDataPostgres(tablename, nid, analysis_date, current_date):
+    """ Retrieve raw hardware data for device nid for the specified time in the
+        given data table in postgres DB
     """
-    # note: int 'ts' is added in order to work around API caching issue
-    payload = {'nid' : nid, 'date' : date, 'ts':int(time.time())}
 
-    header = {"Content-Type": "application/json", 'Authorization' : 'Basic %s' % base64.b64encode(API_key)}
-    req = requests.get("%s/getanalysestatus" % url, params=payload, headers=header)
-    logging.debug("getStatus url: %s" % str(req.url))
-    if req.status_code != requests.codes.ok:
-        raise Exception("getStatus returned http status %d" % req.status_code)
-    resp = req.json()
-    if not resp["success"]:
-        raise Exception("getStatus returned error message %s" % resp["error"])
-    stat = resp["status"]
-    logging.debug("getStatus has value: %s" % str(stat))
-    return stat == 1
-
-
-def getPendingAnalysisDates(url, nid):
-    """Get the dates where no successful analysis has been performed for device nid yet.
-    Return list of dates ('YYYY-MM-dd')
-
-    """
-    # note: int 'ts' is added in order to work around API caching issue
-    payload = {'nid' : nid, 'ts':int(time.time())}
-
-    header = {"Content-Type": "application/json", 'Authorization' : 'Basic %s' % base64.b64encode(API_key)}
-    req = requests.get("%s/getpendinganalysedates" % url, params=payload, headers=header)
-    logging.debug("getPendingAnalysisDates url: %s" % str(req.url))
-    if req.status_code != requests.codes.ok:
-        raise Exception("getPendingAnalysisDates returned http status %d" % req.status_code)
-    resp = req.json()
-    if not resp["success"]:
-        raise Exception("getPendingAnalysisDates returned error message %s" % resp["error"])
-    return resp["dates"]
-
-
-def setStatus(url, nid, date, status):
-    """Set the processed/unprocessed status for device nid on the
-    specified date (date format 'YYYY-MM-dd') using the specified API
-    url. Return boolean to indicate if setting the status was
-    successful.
-
-    """
-    # note: int 'ts' is added in order to work around API caching issue
-    payload = {'nid' : nid, 'date' : date, 'status' : status }
-
-    header = {"Content-Type": "application/json", 'Authorization' : 'Basic %s' % base64.b64encode(API_key)}
-    req = requests.get("%s/updateanalysestatus" % url, params=payload, headers=header)
-    logging.debug("setStatus url: %s" % str(req.url))
-    if req.status_code != requests.codes.ok:
-        raise Exception("setStatus returned http status %d" % req.status_code)
-    resp = req.json()
-    return resp["success"]
-
-
-def saveMode(url, nid, timestamps, modes):
-    """Save predicted travel modes for device nid. Modes is a list of
-    predicted modes, timestamps is the list of timestamps of the
-    measurements. The order of the predicted modes and timestamps must
-    be the same. Return True if successful and False otherwise.
-
-    """
-    header = {"Accept":"application/json","Content-Type": "application/json", 'Authorization' : 'Basic %s' % base64.b64encode(API_key)}
-    if modes is None or timestamps is None or len(modes) != len(timestamps):
-        raise ValueError("modes and timestamps must be of the same length and not None.")
-    to_dict = lambda x: {"timestamp": long(x[0]), "cmode": int(x[1])}
-    # note: int 'ts' is added in order to work around API caching issue
-    payload = {'nid': nid, 'cmodes': map(to_dict, izip(timestamps, modes))}
-    req = requests.post("%s/importcmode" % url, data=json.dumps(payload), headers=header)
-    if req.status_code != requests.codes.ok:
-        logging.warning("saveMode returned http status %d" % req.status_code)
-        return False
-    resp = req.json()
-    # resp["success"] has type bool
-    stat = resp["success"]
-    logging.debug("saveMode has value: %s" % str(stat))
-    return stat
-
-
-def saveTrips(url, nid, date, payload):
-    """Save trips for device nid on the specified date (date format
-    'YYYY-MM-dd') using the specified API url. Trips is a list of
-    identified trips, each trip is dictionary containing the start
-    time, end time, overall travel mode. Return True if successful and
-    False otherwise.
-
-    """
-    if payload == None:
-        logging.warning("When saving trips to backend, payload must not be None.")
-        return False
-
-    payload['nid'] = nid
-    payload['date'] = date
-#    logging.warning(payload)
+    try:
+        conn=psycopg2.connect("dbname='nse' user='postgres' password='Pa8LpCxW' host='54.179.172.158'")
+        print "DB Connected"
+        cur = conn.cursor()
     
-    logging.warning("Saving analytic summary of "+str(nid)+" on "+str(date))
-        
-#    payload = trips.copy()
-#    payload['home_loc'] = [home_loc[0],home_loc[1]]
-#    payload['school_loc'] = [school_loc[0],school_loc[1]]
-#    payload['poi_lat'] = pois['poi_lat']
-#    payload['poi_lon'] = pois['poi_lon']
+    	allQuery = """SELECT * from """+tablename+""" WHERE nid="""+str(nid)+""" AND (ts>='"""+analysis_date+""" 00:00:00' AND ts<'"""+current_date+""" 00:00:00') """
+    	cur.execute(allQuery)
+        dataAll = cur.fetchall()
+        if len(dataAll)>0:
+            rawColumns = zip(*dataAll)
+            raw_ts = rawColumns[2]
+            unix_ts = []
+            for ts in raw_ts:
+                unix_ts.append(calendar.timegm(ts.timetuple())-8*3600) # convert SGT to unix timestamps
 
-    header = {"Accept":"application/json","Content-Type": "application/json", 'Authorization' : 'Basic %s' % base64.b64encode(API_key)}
-    
-    req = requests.post("%s/importanalysedsummary" % url, data=json.dumps(payload), headers=header)
-    
-#    logging.warning("json dump to the API: "+json.dumps(payload))
+            df = pd.DataFrame.from_items([('TIMESTAMP',unix_ts),('MODE',rawColumns[5]),('CMODE',rawColumns[6]), \
+            ('STEPS',rawColumns[9]),('WLATITUDE',rawColumns[13]),('WLONGITUDE',rawColumns[14]),('ACCURACY',rawColumns[15])])
+            return df
+        else:
+            logging.warning('No data!')
+            return None
 
-    if req.status_code != requests.codes.ok:
-        logging.warning("saveTrips returned http status %d" % req.status_code)
-        return False
-    resp = req.json()
-    # resp["success"] has type bool
-    stat = resp["success"]
-#    logging.warning("saveTrips has return value: %s" % str(stat))
-    if not stat:
-        logging.warning("saveTrips returned error message %s" % resp["error"])
-    return stat
-
+    except psycopg2.DatabaseError, e:
+            print e
+            return None
 
 def calculate_features(data_frame, high_velocity_thresh=40):
     """Calculate additional features and attributes from the raw hardware
@@ -204,11 +109,16 @@ def calculate_features(data_frame, high_velocity_thresh=40):
     """
     
     # calculate time delta since the last measurement, in seconds
+    # sort the data frame based on timestamps to get ascending timestamps
+    data_frame=data_frame.sort(['TIMESTAMP'],ascending=[1])
     consec_timestamps = izip(data_frame[['TIMESTAMP']].values[:-1], data_frame[['TIMESTAMP']].values[1:])
     delta_timestamps = map(lambda x: x[1][0]-x[0][0], consec_timestamps)
     # add a zero value for the first measurement where no delta is available
     delta_timestamps = [0] + delta_timestamps
     data_frame['TIME_DELTA'] = pd.Series(delta_timestamps)
+    # ts_series = pd.Series(delta_timestamps)
+    # print ts_series[ts_series<=0]
+    # print data_frame
     
     # calculate steps delta since the last measurement
     consec_steps = izip(data_frame[['STEPS']].values[:-1], data_frame[['STEPS']].values[1:])
@@ -253,6 +163,8 @@ def calculate_features(data_frame, high_velocity_thresh=40):
     idx_large_jump = np.where(np.array(delta_timestamps)>LARGE_TIME_JUMP)[0].tolist()
     ave_velocity_all[idx_large_jump] = velocity_all[idx_large_jump]
     data_frame['AVE_VELOCITY'] = pd.Series(ave_velocity_all.tolist()) # velocity in m/s
+
+    return data_frame
 
 def getFirstSecondOfDay(timestamp):
     """get a unix timestamp in UTC time and return the timestamp for first second of
@@ -307,68 +219,8 @@ def clean_data(data_frame, valid_lat_low=1.0,
         data_frame.loc[invalid_location(data_frame['ACCURACY']) ,
                        ['WLATITUDE', 'WLONGITUDE']] = np.nan
 
-
-def create_geojson(nid, data_frame, home_loc, school_loc):
-    """Create GeoJSON string for one node ID . nid is the node id of the
-    device, the dataframe is the corresponding pandas data frame
-    containing lat/lon locations etc. home_loc and school_loc are
-    (lat, lon) tuples for the identified home and school, respectivly.
-    Return a GeoJSON string that can be copy-pasted to a website like
-    geojson.io for visulaization and inspection.
-
-    """
-    # output a line string for the location data from the device
-    json_output = '''
-    { "type": "Feature",
-        "geometry": { "type": "LineString",
-          "coordinates": [
-             '''
-    # NOTE: GeoJSON coordinates are (lon, lat) not (lat, lon)
-    json_output += ", ".join( ("[%g, %g]" % (point[1], point[0]) \
-                               for point in data_frame[['WLATITUDE', 'WLONGITUDE']].values \
-                               if not (point[0] == None or point[1] == None or \
-                                       np.isnan(point[0]) or np.isnan(point[1])) ) )
-    json_output += '''
-            ]
-          },
-          "properties": {
-            "nid": %d,
-            "name": "location data"
-          }
-     }
-      ''' % nid
-
-    # put markers for home and school location
-    if not (home_loc[0] == None or home_loc[0] == None or \
-            np.isnan(home_loc[0]) or np.isnan(home_loc[1])):
-        json_output += ''',
-         { "type": "Feature",
-           "geometry": {
-              "type": "Point",
-              "coordinates": [%g, %g] },
-           "properties": {
-              "nid": %d,
-              "name": "home"
-            }
-         }''' % (home_loc[1], home_loc[0], nid)
-
-    if not (school_loc[0] == None or school_loc[0] == None or \
-            np.isnan(school_loc[0]) or np.isnan(school_loc[1])):
-        json_output += ''',
-         { "type": "Feature",
-           "geometry": {
-              "type": "Point",
-              "coordinates": [%g, %g] },
-           "properties": {
-              "nid": %d,
-              "name": "school"
-            }
-         }''' % (school_loc[1], school_loc[0], nid)
-
-    return json_output
-
 def main(url, nid, current_date =
-         datetime.date.today().strftime("%Y%m%d"), log_level=logging.WARNING):
+         datetime.date.today().strftime("%Y-%m-%d"), log_level=logging.WARNING):
     """Main processing function, it takes in a nid and provides desired analytic results,
     including raw coordinates along the real track, home/school locations, modes/distance/duration of the am/pm trip
     """
@@ -381,29 +233,32 @@ def main(url, nid, current_date =
         """
 
         # convert analysis_date into unix timestamp in UTC time
-        analysis_unix = calendar.timegm(analysis_date_tuple.timetuple())
+        # analysis_unix = calendar.timegm(analysis_date_tuple.timetuple())
 
         # get the starting and end indices for querying the data, UTC timestamps
 #        start_get = int(getFirstSecondOfDay(analysis_unix))+12*3600 #12 pm of the analysis day
 #        end_get = int(start_get+24*3600-1) #12 pm of the day after the analysis day
-        start_get = int(getFirstSecondOfDay(analysis_unix)) #0 am of the analysis day
-        end_get = int(start_get+24*3600-1) #24 pm of the analysis day
+        # start_get = int(getFirstSecondOfDay(analysis_unix)) #0 am of the analysis day
+        # end_get = int(start_get+24*3600-1) #24 pm of the analysis day
 
         print "current_date: ", current_date
         print "previous_date: ", analysis_date
-        print "start unix: ",start_get
-        print "end unix: ",end_get
+        # print "start unix: ",start_get
+        # print "end unix: ",end_get
 
         # retrieve unprocessed device data from the backend
         logging.info("Get data for device %d on the day %s" % (nid, analysis_date))
-        data_frame = getData(url, nid, start_get, end_get)
+        # data_frame = getDataIHPD(url, nid, start_get, end_get)
+        tablename='week1_new'
+        data_frame = getDataPostgres(tablename, nid, analysis_date, current_date)
+
         if data_frame is None:
             logging.info("No data returned for device %d, skip." % nid)
-            return
+            return None
         elif len(data_frame)<30:
             # if the data frame size is smaller than a certain threshold, then abandon the data
             logging.warning("Too little data returned for device %d, skip." % nid)
-            return
+            return None
 
         # clean data to reduce noise
         logging.info("Clean data for device %d" % nid)
@@ -415,7 +270,9 @@ def main(url, nid, current_date =
                    location_accuracy_thresh=location_accuracy_thresh)
         # calculate additional features
         logging.info("Calculate features for device %d" % nid)
-        calculate_features(data_frame, high_velocity_thresh=high_velocity_thresh)
+        data_frame = calculate_features(data_frame, high_velocity_thresh=high_velocity_thresh)
+        # print 'df after feature calculation'
+        # print data_frame
         # predict the travel mode for each measurement
         logging.info("Predict modes for device %d" % nid)
         hw_modes = data_frame['MODE'].values
@@ -507,8 +364,8 @@ def main(url, nid, current_date =
     start_time = time.time()
 
     # determine which date to process data for which is one day before the current date
-    analysis_date_tuple = datetime.datetime.strptime(current_date, "%Y%m%d") - datetime.timedelta(days=1)
-    analysis_date =  analysis_date_tuple.strftime("%Y%m%d")
+    analysis_date_tuple = datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=1)
+    analysis_date =  analysis_date_tuple.strftime("%Y-%m-%d")
     analysis_weekday = analysis_date_tuple.weekday() # 0 for Monday, 6 for Sunday
     if analysis_weekday==0:
         home_end = 0
@@ -527,10 +384,13 @@ def main(url, nid, current_date =
     logging.info("== Process device ID = %d ==" % nid)
     try:
         result = process(nid, analysis_date, current_date)
+        if result==None:
+            logging.warning('No analytic results gotten!')
+            return None
     except:
         e = traceback.format_exc()
         logging.error("Processing nid %d failed: %s" % (nid, e))
-        return null
+        return None
     
     logging.info("---Processed data for node %d in %.2f seconds ---" % (nid, time.time() - start_time))
     
